37213a94ff1aa9182f5938f1593f6d3cf0d6056d
강수진 <rkdthd1234@naver.com>
2020-03-30 23:27:24 +0900

    Create Initial Project

---
+++ HorrorMovieBlinder/HorrorMovieBlinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HorrorMovieBlinder
+//
+//  Created by 강수진 on 2020/03/30.
+//  Copyright © 2020 강수진. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
